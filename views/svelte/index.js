55bc821cc3d95470a93837fe2f341566488c12c4
Sebastian Carro <88806181+artifice911@users.noreply.github.com>
2023-07-07 09:15:19 -0300

    CAMBIOmEOPP

--- views/svelte/index.js
+++ views/svelte/index.js
@@ -4,6 +4,9 @@ import Footer from './index/Footer.svelte';
 import Header from './index/Header.svelte';
 import Formulario from './index/Formulario.svelte';
 import H1 from './index/H1.svelte';
+import Nav from './index/Nav.svelte';
 
-section(Formulario);
+header(Nav);
+
+footer(Footer);
 
---
+++ views/svelte/index/Nav.svelte
@@ -0,0 +1,99 @@
+<section class="container">
+  <div class="row text-center ps-0 pe-0 contenedor">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <ul class="d-grid gap-2 mx-auto nav nav-tabs;" role="tablist">
+        <li>
+          <a href="#"><span>Institucional </span></a>
+        </li>
+        <li><a href="#"><span>Formularios</span></a></li>
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <ul class="d-grid gap-2 mx-auto nav nav-tabs" role="tablist">
+        <li>
+          <a href="#"><span>Cotizaciones y Afiliaciones</span></a>
+        </li>
+        <li>
+          <a href="#"><span>Preguntas Frecuentes</span></a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <ul class="d-grid gap-2 mx-auto nav nav-tabs" role="tablist">
+        <li><a href="#"><span>Normativa</span></a></li>
+        <li><a href="#"><span>Prestadores</span></a></li>
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+      <ul class="d-grid gap-2 mx-auto nav nav-tabs" role="tablist">
+        <li>
+          <a href="#"><span>Centro de Formación</span></a>
+        </li>
+        <li>
+          <a href="#"><span>Consultas y Reclamos</span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+<section class="navbar navbar-expand-sm bg-body-tertiary rounded">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Centered nav only</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+</section>
+
+
+
+<style>
+  .contenedor {
+    padding: 15px;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 2px 2px 4px rgba(44, 43, 43, 0.7);
+    margin: 15px;
+  }
+
+  li {
+    border: 2px 2px;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  li:hover {
+    background-color: rgb(118, 219, 106);
+  }
+
+  span:hover {
+    color: white;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+</style>
