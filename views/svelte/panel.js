5fc92c0aa3b5ba5caee4ce78f7988094f4cb618e
Josué <escobarjosue03@gmail.com>
2026-03-06 15:33:23 -0300

    deploy 958031

--- views/svelte/panel.js
+++ views/svelte/panel.js
@@ -27,10 +27,10 @@ menu('Sitio', {icon:"globe"});
 menu('Sitio/Cuota inicial Jubilados', e=>{
 	variable(VAR_JUBILADOS_CUOTA_INICIAL)
     .then(p=>input("Editar cuota inicial, hasta el 15 de cada mes (número con punto decimal)",p)
-        .accept(p=>variable(VAR_JUBILADOS_CUOTA_INICIAL,p)))
+        .accept(p=>variable(VAR_JUBILADOS_CUOTA_INICIAL, set => p)))
 }, {icon:"globe"});
 menu('Sitio/Cuota tardia Jubilados', e=>{
 	variable(VAR_JUBILADOS_CUOTA_TARDIA)
     .then(p=>input("Editar cuota tardia a pagos a partir del 15 de cada mes (número con punto decimal)",p)
-        .accept(p=>variable(VAR_JUBILADOS_CUOTA_TARDIA,p)))
+        .accept(p=>variable(VAR_JUBILADOS_CUOTA_TARDIA, set => p)))
 }, {icon:"globe"});
